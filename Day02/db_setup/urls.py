5f671e6b4225e30cfd3f471307a9e966512d86c0
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2024-11-11 00:01:17 +0500

    day 03 code

--- Day02/db_setup/urls.py
+++ Day02/db_setup/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('view/', views.view_products, name='view_products'),
+    path('edit/<int:id>/', views.update_product, name='update_product'),
+    path('delete/<int:id>/', views.delete_product, name='delete_product'),
 ]
--- Day02/db_setup/views.py
+++ Day02/db_setup/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductForm
 from .models import Product
 
-# Create your views here.
+# View for adding a new product
 
 
 def add_product(request):
@@ -10,12 +10,33 @@ def add_product(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'product.html', {'form': form})
+            return redirect('view_products')
     else:
         form = ProductForm()
     return render(request, 'product.html', {'form': form})
+
+# View for listing all products
 
 
 def view_products(request):
     products = Product.objects.all()
     return render(request, 'view_products.html', {'products': products})
+
+# View for updating an existing product
+
+
+def update_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('view_products')
+    return render(request, 'edit_product.html', {'form': form, 'product': product})
+
+# View for deleting a product
+
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    return redirect('view_products')
